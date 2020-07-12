a51adfc032fd9b7fcdcdfeee35f427a8489c0a17
wuyiccc666 <56572026+wuyiccc@users.noreply.github.com>
2020-07-12 20:46:55 +0800

    1. IOC核心注解的创建

---
+++ src/main/java/com/wuyiccc/helloframework/core/annotation/Controller.java
@@ -0,0 +1,16 @@
+package com.wuyiccc.helloframework.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author wuyiccc
+ * @date 2020/7/12 20:28
+ * 岂曰无衣，与子同袍~
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+}
